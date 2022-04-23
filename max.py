328f8b037c22004f19f6bf6dc05cea2587d189f2
Langwenchong <1422257646@qq.com>
2022-04-23 15:55:53 +0800

    max函数完善版

--- max.py
+++ max.py
@@ -4,10 +4,12 @@ import base64
 import numpy as np
 def handler (event, context):
     s=json.loads(str(base64.b64decode(json.dumps(event["body"])),encoding="utf-8"))
-    a=np.array(s['list'])
+    # 输入的数据中None都转换为nan，同时数据都变为了浮点型
+    a=np.array(s['data'],dtype=np.float)
     k=s['k']
-    ans=np.max(a,axis=k)
-    result={'ans': ans.tolist()}
+    # 取最大值
+    ans=process(np.max(a,axis=k))
+    result={ 'ans': ans}
     return {
         "statusCode": 200,
         "isBase64Encoded": False,
@@ -16,3 +18,19 @@ def handler (event, context):
             "Content-Type": "application/json"
         }
     }
+
+def process(x):
+    shape=np.shape(x)
+    if(len(shape)==0):
+        return "None" if np.isnan(x) else str(x)
+    elif(len(shape)==1):
+        return [None if np.isnan(i) else i for i in x]
+    elif(len(shape)==2):
+        # 对二维向量中的nan进行替换为None
+        # 取出每一行
+        ans=[]
+        for row in x:
+            ans.append([None if np.isnan(i) else i for i in row])
+        return ans
+            
+        
